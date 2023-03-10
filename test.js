22d069add064e461f1538da57bc98d3fbd89ee8e
Islam Sani <islammuhd8@gmail.com>
2023-03-10 16:06:04 +0100

    added html and js files

---
+++ test.js
@@ -0,0 +1,31 @@
+// let islam = 27;
+// let year = 2023;
+
+// log things to console
+// console.log (islam, year);
+
+// islam = 28;
+// console.log (islam);
+
+// const arsenal = 70;
+// console.log(arsenal);
+
+// var score = 75;
+// console.log(score)
+
+
+//strings
+console.log('hello, world');
+
+let email = 'islammuhd8@gmail.com';
+console.log(email);
+
+//string concatenation
+let firstName = 'Islam';
+let lastName = 'Sani';
+
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+
