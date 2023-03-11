0b7e3ed12e4bdfbfa7ae09fb5f9edd1f64dc8850
Islam Sani <islammuhd8@gmail.com>
2023-03-11 12:17:27 +0100

    worked on concatenate and teemplate string

--- test.js
+++ test.js
@@ -15,30 +15,106 @@
 
 
 //strings
-console.log('hello, world');
+// console.log('hello, world');
 
-let email = 'islammuhd8@gmail.com';
-console.log(email);
+// let email = 'islammuhd8@gmail.com';
+// console.log(email);
 
 //string concatenation
-let firstName = 'Islam';
-let lastName = 'Sani';
+// let firstName = 'Islam';
+// let lastName = 'Sani';
 
-let fullName = firstName + ' ' + lastName;
+// let fullName = firstName + ' ' + lastName;
 
-console.log(fullName);
+// console.log(fullName);
 
 // getting characters
-console.log(fullName[0]);
+// console.log(fullName[0]);
 
 //string length
-console.log(fullName.length);
+// console.log(fullName.length);
 
 //string methods
-console.log(fullName.toUpperCase()); 
-let result = fullName.toLowerCase();
-console.log(result, fullName);
+// console.log(fullName.toUpperCase()); 
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
  
 
+// COMMON STRINGS METHODS
+
+//let email = 'islammuhd8@gmail.com';
+// let result = email.lastIndexOf('a');
+
+//let result = email.slice(0,5);
+
+//let result = email.substr(4,10);
+
+//let result = email.replace('i', 's');
 
 
+//console.log(result);
+
+
+//DATA TYPE "NUMBERS"
+
+//let radius = 10;
+//const pi = 3.14;
+
+//console.log(radius, pi); 
+
+//MATH OPERATORS +, -, *, /, **, %
+
+//console.log(10 / 2);
+//let result = radius % 3;
+//let result = pi * radius **2;
+
+//Order of operation - B I D M A S
+
+//let result = 5 * (10-3) **2; 
+
+//console.log(result);
+
+//let likes = 10;
+
+//likes = likes + 1;
+//likes++;
+//likes +=10;
+//likes -=5;
+//likes *=10;
+//likes /=2;
+//likes--;
+
+//console.log(likes);
+
+//NAN = not a number
+
+// console.log(4 * "islam");
+// console.log(4 / "islam");
+
+//let result = 'islam hit ' + likes + ' likes on twitter';
+//console.log(result);
+
+//TEMPLATE STRINGS: this allows us to inject variables into the string without having to exit out and using the plus (+) sign to cancatenate them.
+
+const title = 'Best footballers of all time';
+const author = 'Islam';
+const likes = 20;
+
+//CONCATENATION WAY:
+//let result = 'The blog called ' + title + ' by ' + author + ' has ' + 10 + ' likes ';
+//console.log(result);
+
+
+//TEMPLATE STRING WAY or template literal:
+//let result = `The blog called ${title} by ${author} has ${likes}`;
+//console.log(result);
+
+//creating html template
+
+//let html = `
+//    <h2>${title}</h2>
+//    <p>by ${author}</p>
+ //   <span>This blog has ${likes} likes</span>
+//`;
+
+//console.log(html);
